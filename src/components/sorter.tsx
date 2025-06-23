cacf5761631c779e7e6fa53a89ebefedbdf47807
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-23 15:42:42 -0400

    Setup basic sort controls

--- src/components/sorter.tsx
+++ src/components/sorter.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import clsx from 'clsx'
 
-import { Play, StepForward } from 'lucide-react'
+import { Play, OctagonX, StepForward } from 'lucide-react'
 import { Button } from 'src/components/ui/button'
 import { Input } from 'src/components/ui/input'
+import { toast } from 'sonner'
 
 import { wrapArray, stepMergeSort } from '@/lib/mergesort'
 import type { State } from '@/lib/mergesort'
@@ -12,32 +14,32 @@ import type { State } from '@/lib/mergesort'
 export function Sorter() {
   const [sortState, setSortState] = useState<State | undefined>(undefined)
   const [input, setInput] = useState<string>('7, 4, 3, 5, 2, 1, 6')
-  const [sorting, setSorting] = useState(false)
 
   function newSortState() {
     // validate the input string
     const ok = /^\s*\d+\s*(,\s*\d+\s*)*$/.test(input);
     if (!ok) {
-      // call the toast here
-      console.log('invalid input')
+      toast.warning("invalid input, please use numbers separated by commas")
     } else {
       const newState: State = {
         arr: wrapArray((input.split(',')).map(num => Number(num.trim()))),
         act: 'split'
       }
-      setSorting(true)
       setSortState(newState)
     }
   }
 
   function iterateSortState() {
-    if (!sortState || !sorting) {
-      // call the toast here
-      console.log('no valid sort state!')
+    if (!sortState) {
+      toast.warning('no valid sort state, cannot iterate')
     } else {
       setSortState(stepMergeSort(sortState))
       console.log('sortState')
     }
+  }
+
+  function haltSort() {
+    setSortState(undefined)
   }
 
   useEffect(() => {
@@ -46,6 +48,7 @@ export function Sorter() {
 
   return (
     <div className="flex border-2 rounded bg-amber-200 flex-col size-2/3 gap-12 m-6 p-6">
+
       {/* control row */}
       <div className="flex gap-4 ">
         <Input
@@ -53,10 +56,18 @@ export function Sorter() {
           value={input}
           onChange={(e) => { setInput(e.target.value) }}
         />
-        <Button size='icon' onClick={newSortState}>
-          <Play />
+        <Button
+          className={sortState ? 'bg-red-400' : 'bg-green-400'}
+          size='icon'
+          onClick={sortState ? haltSort : newSortState}
+        >
+          {sortState ? <OctagonX /> : <Play />}
         </Button>
-        <Button size='icon' onClick={iterateSortState}>
+
+        <Button
+          size='icon'
+          onClick={iterateSortState}
+        >
           <StepForward />
         </Button>
       </div>
