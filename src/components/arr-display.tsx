649f613aa9fd7f45575d01c22d958e1f0185f915
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-23 16:43:13 -0400

    Cleanup

--- src/components/arr-display.tsx
+++ src/components/arr-display.tsx
@@ -2,9 +2,29 @@ import type { State } from '@/lib/mergesort'
 
 export function ArrDisplay({ state }: { state: State | undefined }) {
 
+  function mapMainArray(arr: number[][]) {
+    return (
+      <div className="flex gap-8">
+        {
+          arr.map((arr, idx) => {
+            if (arr.length !== 0) {
+              return (
+                <div id={idx.toString()}>
+                  {mapSubArray(arr)}
+                </div>
+              )
+            } else {
+              return undefined
+            }
+          })
+        }
+      </div>
+    )
+  }
+
   function mapSubArray(arr: number[]) {
     return (
-      <div className="flex border-2 rounded-2xl bg-amber-200 p-4 gap-6">
+      <div className="flex border-2 rounded-2xl bg-amber-200 p-4 gap-4">
         {arr.map((num, idx) => {
           return (
             <div className="border-2 rounded-2xl p-2 bg-amber-400" id={idx.toString()}>
@@ -24,20 +44,10 @@ export function ArrDisplay({ state }: { state: State | undefined }) {
     )
   } else {
     return (
-      <div className="flex gap-8">
-        {
-          state.arr.map((arr, idx) => {
-            if (arr.length !== 0) {
-              return (
-                <div id={idx.toString()}>
-                  {mapSubArray(arr)}
-                </div>
-              )
-            } else {
-              return undefined
-            }
-          })
-        }
+      <div>
+        <div className="flex flex-col gap-8">
+          {mapMainArray(state.arr)}
+        </div>
       </div>
     )
 
