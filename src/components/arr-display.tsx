18ed0e4a7b404affa98751d6cf198bb4ebb543ea
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-23 16:37:53 -0400

    Wrap values being sorted

--- src/components/arr-display.tsx
+++ src/components/arr-display.tsx
@@ -1,6 +1,21 @@
 import type { State } from '@/lib/mergesort'
 
 export function ArrDisplay({ state }: { state: State | undefined }) {
+
+  function mapSubArray(arr: number[]) {
+    return (
+      <div className="flex border-2 rounded-2xl bg-amber-200 p-4 gap-6">
+        {arr.map((num, idx) => {
+          return (
+            <div className="border-2 rounded-2xl p-2 bg-amber-400" id={idx.toString()}>
+              {num}
+            </div>
+          )
+        })}
+      </ div>
+    )
+  }
+
   if (!state) {
     return (
       <div>
@@ -11,12 +26,16 @@ export function ArrDisplay({ state }: { state: State | undefined }) {
     return (
       <div className="flex gap-8">
         {
-          state.arr.map((a, idx) => {
-            return (
-              <div id={idx.toString()}>
-                {a}
-              </div>
-            )
+          state.arr.map((arr, idx) => {
+            if (arr.length !== 0) {
+              return (
+                <div id={idx.toString()}>
+                  {mapSubArray(arr)}
+                </div>
+              )
+            } else {
+              return undefined
+            }
           })
         }
       </div>
