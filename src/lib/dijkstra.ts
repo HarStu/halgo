dfc68f1699441d5568388cbb79afa9ffbf30cf01
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-25 10:41:30 -0400

    Implement binary heap

---
+++ src/lib/dijkstra.ts
@@ -0,0 +1,73 @@
+export type Node = {
+  id: string
+}
+
+export type Edge = {
+  from: string,
+  to: string,
+  weight: number
+}
+
+export type Graph = {
+  nodes: Node[],
+  edges: Edge[]
+}
+
+const graph = {
+  nodes: [
+    { id: "a" },
+    { id: "b" },
+    { id: "c" },
+    { id: "d" },
+    { id: "e" },
+    { id: "f" },
+    { id: "g" },
+    { id: "h" },
+    { id: "i" },
+    { id: "j" },
+    { id: "k" }
+  ],
+  edges: [
+    { from: "a", to: "b", weight: 3 },
+    { from: "a", to: "d", weight: 9 },
+    { from: "d", to: "c", weight: 4 },
+    { from: "b", to: "e", weight: 9 },
+    { from: "c", to: "f", weight: 6 },
+    { from: "c", to: "i", weight: 2 },
+    { from: "e", to: "f", weight: 5 },
+    { from: "f", to: "g", weight: 5 },
+    { from: "f", to: "h", weight: 2 },
+    { from: "h", to: "j", weight: 3 },
+    { from: "g", to: "k", weight: 5 },
+    { from: "j", to: "k", weight: 6 },
+  ]
+}
+
+function heapInsert(heap: number[], val: number) {
+  // add new element
+  heap.push(val)
+
+  // adjust the heap
+  return balanceHeap(heap)
+}
+
+function balanceHeap(heap: number[]) {
+  for (let idx = heap.length - 1; idx > 0; idx++) {
+    const parentIdx = ((idx - ((idx % 2) ? 1 : 2))) / 2
+
+    if (heap[parentIdx]! > heap[idx]!) {
+      const val = heap[idx]
+      heap[idx] = heap[parentIdx]!
+      heap[parentIdx] = val!
+      balanceHeap(heap)
+    }
+  }
+  return heap
+}
+
+function dijkstra(graph: Graph) {
+  const visited = graph.nodes.map(node => ({ id: node.id, distanceTo: Infinity }))
+  // start at the first node
+
+  let currentNode = graph.nodes[0]
+}
